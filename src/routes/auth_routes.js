43278967cc86b344d81ede838c694a7353f7c150
Keona Hicks <keona2003hicks@gmail.com>
2024-12-14 17:38:41 -0500

    Restructured the Views directories and updated the render paths in auth_routes to properly link the sign up page (so the nav bar does not appear on the sign in page)

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,53 +1,46 @@
-import e, {Router} from 'express';
+import e, { Router } from "express";
 const router = Router();
 import { users } from "../config/mongoCollections.js";
 import bcrypt from "bcrypt";
 
+router.route("/").get(async (req, res) => {
+  console.log("Hello World");
+  res.redirect("/intro");
+});
 
-router.route('/').get(async (req, res) => {
-    console.log("Hello World");
-      res.redirect('/intro');
-  });
-  
+router.route("/intro").get(async (req, res) => {
+  res.render("intro", { title: "Sign In", layout: "sign_up_layout" });
+});
 
-  router
-  .route('/intro')
-  .get(async (req, res) => {
-    res.render('layouts/intro', {title: "Sign Up"});
-  });
+router.route("/signup").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    showAuthenticatedPage(req, res);
+  } else {
+    res.render("sign_up", { layout: "sign_up_layout" });
+  }
+});
 
-  router
-  .route('/signup')
-  .get(async (req, res) => {
-    if (req.session && req.session.AuthenticationState) {
-      showAuthenticatedPage(req, res);
+router.route("/auth/login").post(async (req, res) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ email: req.body.email });
+  if (!user) {
+    res.status(401).send("User not found");
+    return;
+  } else {
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password_hash
+    );
+    if (passwordMatch) {
+      req.session.AuthenticationState = {
+        username: user.username,
+        _id: user._id,
+      };
+      res.render("homepage", { layout: "main", user: user });
     } else {
-      res.render('layouts/sign_up_layout');
+      res.status(401).send("Password does not match");
     }
-  })
+  }
+});
 
-  router
-  .route('/auth/login')
-  .post(async (req, res) => {
-    const userCollection = await users();
-    const user = await userCollection.findOne({ email: req.body.email });
-    if (!user) {
-      res.status(401).send("User not found");
-      return;
-    } else {
-      const passwordMatch = await bcrypt.compare(req.body.password, user.password_hash);
-      if (passwordMatch) {
-        req.session.AuthenticationState = {
-          username: user.username,
-          _id: user._id,
-        };
-        res.render('layouts/homepage', user);
-      } else {
-        res.status(401).send("Password does not match");
-      }
-    }
-  })
-
-
-  export default router;  
-
+export default router;
