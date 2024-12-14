2b1fbc3cec10df211a53673400ec0b323286eccf
Keona Hicks <keona2003hicks@gmail.com>
2024-12-14 18:13:33 -0500

    Added a sign up page and updated auth_routes to match Alice's changes in branch

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,7 +1,15 @@
 import e, { Router } from "express";
 const router = Router();
-import { users } from "../config/mongoCollections.js";
+import { users, videos } from "../config/mongoCollections.js";
 import bcrypt from "bcrypt";
+import validation from "../utils/validation.js";
+
+const getUserByEmail = async (email) => {
+  const userCollection = await users();
+  const user = await userCollection.findOne({ email: email });
+  if (!user) throw new Error("User not found");
+  return user;
+};
 
 router.route("/").get(async (req, res) => {
   console.log("Hello World");
@@ -9,38 +17,87 @@ router.route("/").get(async (req, res) => {
 });
 
 router.route("/intro").get(async (req, res) => {
-  res.render("intro", { title: "Sign In", layout: "sign_up_layout" });
+  if (req.session && req.session.AuthenticationState) {
+    res.redirect("/homepage");
+  } else {
+    return res.render("intro", {
+      title: "Sign Up",
+      layout: "sign_up_layout",
+    });
+  }
 });
 
 router.route("/signup").get(async (req, res) => {
   if (req.session && req.session.AuthenticationState) {
-    showAuthenticatedPage(req, res);
+    res.redirect("/homepage");
   } else {
-    res.render("sign_up", { layout: "sign_up_layout" });
+    res.render("sign_up", {
+      layout: "sign_up_layout",
+    });
   }
 });
 
-router.route("/auth/login").post(async (req, res) => {
-  const userCollection = await users();
-  const user = await userCollection.findOne({ email: req.body.email });
-  if (!user) {
-    res.status(401).send("User not found");
-    return;
-  } else {
-    const passwordMatch = await bcrypt.compare(
-      req.body.password,
-      user.password_hash
-    );
-    if (passwordMatch) {
-      req.session.AuthenticationState = {
-        username: user.username,
-        _id: user._id,
-      };
-      res.render("homepage", { layout: "main", user: user });
-    } else {
-      res.status(401).send("Password does not match");
+router
+  .route("/auth/logout")
+  .post(async (req, res) => {
+    if (req.session && req.session.AuthenticationState) {
+      req.session.destroy();
     }
+    return res.redirect("/intro");
+  })
+  .get(async (req, res) => {
+    return res.redirect("/intro");
+  });
+
+router.route("/homepage").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    const v = {}; //await videos.getVideoByOwner( req.session.AuthenticationState.user._id)
+    // Updated to use correct path
+    res.render("homepage", {
+      layout: "main",
+      user: req.session.AuthenticationState.user,
+    });
+  } else {
+    return res.status(401).redirect("/intro");
   }
 });
+
+router.route("/profile").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    // Updated to use correct path
+    res.render("profile", {
+      layout: "main",
+      user: req.session.AuthenticationState.user,
+    });
+  } else {
+    return res.status(401).redirect("/intro");
+  }
+});
+
+router
+  .route("/settings")
+  .get(async (req, res) => {
+    if (req.session && req.session.AuthenticationState) {
+      // Updated to use correct path
+      return res.render("settings", {
+        layout: "main",
+        user: req.session.AuthenticationState.user,
+      });
+    } else {
+      return res.status(401).redirect("/intro");
+    }
+  })
+  .post(async (req, res) => {
+    if (req.session && req.session.AuthenticationState) {
+      users.updateUserPatch(req.session.AuthenticationState.user._id, req.body);
+      // Updated to use correct path
+      res.render("settings", {
+        layout: "main",
+        user: req.session.AuthenticationState.user,
+      });
+    } else {
+      return res.status(401).redirect("/intro");
+    }
+  });
 
 export default router;
