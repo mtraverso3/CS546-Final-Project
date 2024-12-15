d79a371da20f7d3e20d20a3b8995885880610da5
Megan Presbitero <mpresbitero@anl.gov>
2024-12-15 17:34:53 -0500

    added age, and date validation

--- src/data/index.js
+++ src/data/index.js
@@ -1,9 +1,7 @@
 import videoDataFunctions from "./videos.js";
 import userDataFunctions from "./users.js";
 import commentsFunctions from "./comments.js";
-import likesFunctions from "./likes.js";
 
 export const userData = userDataFunctions;
 export const videoData = videoDataFunctions;
 export const commentsData = commentsFunctions;
-export const likesData = likesFunctions
