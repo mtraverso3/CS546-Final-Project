52f7d02fde9d56ae713b4b04cf247404450fff6c
Marcos Traverso <marcostraverso2003@gmail.com>
2024-12-15 15:36:31 -0500

    Add likes to data layer

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,9 @@
 import videoDataFunctions from "./videos.js";
 import userDataFunctions from "./users.js";
 import commentsFunctions from "./comments.js";
+import likesFunctions from "./likes.js";
 
 export const userData = userDataFunctions;
 export const videoData = videoDataFunctions;
 export const commentsData = commentsFunctions;
+export const likesData = likesFunctions
