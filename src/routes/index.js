d012352eb8df0d9f7428aeb90dc6e5183bcafa8e
Marcos Traverso <marcostraverso2003@gmail.com>
2024-12-15 16:44:48 -0500

    Refactor and clean up routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
-// import exampleRoutes from './examples.js';
 import { static as staticDir } from "express";
-import authRoutes from './auth_routes.js';
+import authRoutes from "./auth_routes.js";
+import internalRoutes from "./internal_routes.js";
 
 const constructorMethod = (app) => {
-  app.use('/', authRoutes);
+  app.use("/", authRoutes);
+  app.use("/", internalRoutes);
 
   app.use("/public", staticDir("src/public"));
   app.use("*", (req, res) => {
