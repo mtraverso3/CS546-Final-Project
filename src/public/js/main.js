8e532637f46a27b3b89b80d41ee167a1ddd2275f
Megan Presbitero <mpresbitero@anl.gov>
2024-12-14 21:41:45 -0500

    fixed and added validation for signup-form

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,151 +1,350 @@
 // let firstName = document.getElementById('firstName');
+// let lastName = document.getElementById('lastName');
+// let userId = document.getElementById('userId');
 // let confirmPassword = document.getElementById('confirmPassword');
 // let favoriteQuote = document.getElementById('favoriteQuote');
 // let fontColor = document.getElementById('fontColor');
 // let backgroundColor = document.getElementById('backgroundColor');
 // let role = document.getElementById('role');
 
-const validateString = (strVal, varName) =>{
-    console.log(strVal)
-    if (!strVal) throw `Error: You must supply a ${varName}!`;
-    if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
-    strVal = strVal.trim();
-    if (strVal.length === 0)
-        throw `Error: ${varName} cannot be an empty string or string with just spaces`;
-    if (!isNaN(strVal))
-        throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
-    return strVal;
-}
+// (function(){
 
-function checkEmail(email) {
-    email = validateString(email, "email")
-    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return regex.test(email);
-}
+//     const validateString = (strVal, varName) =>{
+//         console.log(strVal)
+//         if (!strVal) throw `Error: You must supply a ${varName}!`;
+//         if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
+//         strVal = strVal.trim();
+//         if (strVal.length === 0)
+//             throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+//         if (!isNaN(strVal))
+//             throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+//         return strVal;
+//     }
+//     const checkName = (name) =>{
+//         name = validateString(name)
+//         if(name.length > 25 || name.length < 2){
+//             throw "name must be between 2 - 25 characters"
+//         }
+//         const regex = /\d/;
+//         if(regex.test(name)){
+//             throw "name cannot contain numbers"
+//         }
+//         return name
+//     }
 
-let errorDiv = document.getElementById('error-div');
-errorDiv.hidden = true;
-errorDiv.innerHTML = '';
+//     const checkUserId = (userId) => {
+//         userId = validateString(userId)
+//         if(userId.length > 10 || userId.length < 5){
+//             throw "UserID must be between 5 - 10 characters"
+//         }
+//         return userId
+//     }
 
-let rememberMe = document.getElementById('remember-me');
-let email = document.getElementById('email');
-let password = document.getElementById('password');
+//     function checkEmail(email) {
+//         email = validateString(email, "email")
+//         const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//         if(!regex.test(email)){
+//             throw "email must be a valid email of the form: example@domain.com"
+//         }
+//         return email;
+//     }
+//     const checkPassword = (password) => {
+//         console.log("in password check")
+//         password = validateString(password)
+//         if(password.length < 8){
+//             throw "Password must be at least 8 characters long"
+//         }
+//         let regex1 = /[A-Z]/
+//         let regex2 = /[^A-Za-z0-9]/
+//         let regex3 = /\d/
+//         if(!regex1.test(password)){
+//             throw "Password must have at least 1 Uppercase letter"
+//         }
+//         if(!regex2.test(password)){
+//             throw "Password must contain at least 1 special character"
+//         }
+//         if(!regex3.test(password)){
+//             throw "Password must have at least 1 number"
+//         }
+//         return password
+        
+//     }
+//     console.log("javascript")
 
-let search_container = document.getElementById('search-container')
-let header = document.getElementById('header-actions')
-
-let signUpForm = document.getElementById('signup-form');
-let loginForm = document.getElementById('login-form');
+//     let errorDiv = document.getElementById('error-div');
 
 
-// if (signUpForm) {
-//     let errors = [];
-//     signUpForm.addEventListener('submit', (event) => {
-//         event.preventDefault();
-    
-//     errors = [];
-//     errorDiv.classList.add('hidden');
-//     errorDiv.innerHTML = '';
-//     console.log("listening")
-//     try {
-//         firstName.value = checkName(firstName.value);
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     try {
-//         lastName.value = checkName(lastName.value);
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     try {
-//         userId.value = checkUserId(userId.value);
-//         userId.value = userId.value.toLowerCase();
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     try {
-//         password.value = checkPassword(password.value);
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     try {
-//         confirmPassword.value = checkPassword(confirmPassword.value);
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     if (password.value !== confirmPassword.value) {
-//         errors.push('Error: Password and Confirm Password must match');
-//     }
-//     try {
-//         favoriteQuote.value = checkFavQuote(favoriteQuote.value);
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     const theme = {backgroundColor: backgroundColor.value, fontColor: fontColor.value}
-//     try {
-//         let themePreference = checkThemePref(theme);
-//         backgroundColor.value = themePreference.backgroundColor;
-//         fontColor.value = themePreference.fontColor;
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     try {
-//         role.value = checkString(role.value);
-//         role.value = role.value.toLowerCase();
-//         if (role.value !== 'admin' && role.value !== 'user'){
-//             errors.push('Role must be either "admin" or "user"')
-//         } 
-//     } catch (e) {
-//         errors.push(e.message);
-//     }
-//     if(errors.length > 0) {
-//         console.log("test a")
-//         event.preventDefault()
-//         let myUL = document.createElement('ul');
-//         for (let i = 0; i < errors.length; i++) {
-//             let myLi = document.createElement('li');
-//             myLi.classList.add('error-div');
-//             myLi.innerHTML = errors[i];
-//             myUL.appendChild(myLi);
-//           }
-//           errorDiv.appendChild(myUL);
+//     let firstName = document.getElementById('firstName');
+//     let lastName = document.getElementById('lastName');
+//     let userId = document.getElementById('userId');
+//     let enterEmail = document.getElementById('enterEmail');
+//     let password = document.getElementById('password');
+//     let confirmPassword = document.getElementById('confirmPassword');
 
-//         errorDiv.classList.remove('hidden');
+//     let rememberMe = document.getElementById('remember-me');
+//     let email = document.getElementById('email');
+
+//     let search_container = document.getElementById('search-container')
+//     let header = document.getElementById('header-actions')
+
+//     let signUpForm = document.getElementById('signup-form');
+//     let loginForm = document.getElementById('login-form');
+
+
+//     if (signUpForm) {
+//         let errors = [];
+//         //errorDiv.hidden = true;
+//         //errorDiv.innerHTML = '';
+//         signUpForm.addEventListener('submit', (event) => {
+//             event.preventDefault();
+        
+//             errors = [];
+//             errorDiv.classList.add('hidden');
+//             errorDiv.innerHTML = '';
+//             try {
+//                 firstName.value = checkName(firstName.value);
+//             } catch (e) {
+//                 errors.push(e);
+//             }
+//             try {
+//                 lastName.value = checkName(lastName.value);
+//             } catch (e) {
+//                 errors.push(e);
+//             }
+//             try {
+//                 userId.value = checkUserId(userId.value);
+//                 console.log(userId.value)
+//                 userId.value = userId.value.toLowerCase();
+//             } catch (e) {
+//                 errors.push(e);
+//             }
+//             try {
+//                 password.value = checkPassword(password.value);
+//             } catch (e) {
+//                 errors.push(e);
+//             }
+//             try {
+//                 confirmPassword.value = checkPassword(confirmPassword.value);
+//             } catch (e) {
+//                 errors.push(e);
+//             }
+//             if (password.value !== confirmPassword.value) {
+//                 errors.push('Error: Password and Confirm Password must match');
+//             }
+//             if(errors.length > 0) {
+//                 event.preventDefault()
+//                 let myUL = document.createElement('ul');
+//                 for (let i = 0; i < errors.length; i++) {
+//                     let myLi = document.createElement('li');
+//                     myLi.classList.add('error');
+//                     myLi.innerHTML = errors[i];
+//                     myUL.appendChild(myLi);
+//                 }
+//                 errorDiv.appendChild(myUL);
+//                 errorDiv.classList.remove('hidden');
+//                 errorDiv.hidden = false
+//             }
+//         });
 //     }
-//     });
-// }
 
-if (loginForm) {
-    let errors = [];
+//     if (loginForm) {
+//         let errors = [];
+//         console.log("login form")
 
-    signUpForm.addEventListener('submit', (event) => {
-    errors = [];
-    errorDiv.classList.add('hidden');
-    errorDiv.innerHTML = '';
-    try {
-        email.value = checkEmail(email.value);
-        email.value = email.value.toLowerCase();
-    } catch (e) {
-        errors.push(e.message);
+//         //errorDiv.hidden = true;
+//         //errorDiv.innerHTML = '';
+
+//         loginForm.addEventListener('submit', (event) => {
+//             console.log("event listener")
+
+//             errors = [];
+//             errorDiv.classList.add('hidden');
+//             errorDiv.innerHTML = '';
+//             try {
+//                 console.log("in email try")
+//                 email.value = checkEmail(email.value);
+//                 email.value = email.value.toLowerCase();
+//             } catch (e) {
+//                 console.log("Error found")
+//                 errors.push(e);
+//             }
+//             try {
+//                 console.log("in password try")
+
+//                 password.value = checkPassword(password.value);
+//             } catch (e) {
+//                 console.log("error found")
+//                 errors.push(e);
+//             }
+//             if(errors.length > 0) {
+
+//                 event.preventDefault()
+//                 let myUL = document.createElement('ul');
+//                 for (let i = 0; i < errors.length; i++) {
+//                     let myLi = document.createElement('li');
+//                     myLi.classList.add('error');
+//                     myLi.innerHTML = errors[i];
+//                     myUL.appendChild(myLi);
+//                 }
+//                 errorDiv.appendChild(myUL);
+//                 errorDiv.classList.remove('hidden');
+//                 errorDiv.hidden = false
+//             }
+//         });
+//     }
+// })
+
+
+
+
+(function () {
+    validateString = (strVal, varName) =>{
+        console.log(strVal)
+        if (!strVal) throw `Error: You must supply a ${varName}!`;
+        if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
+        strVal = strVal.trim();
+        if (strVal.length === 0)
+            throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+        if (!isNaN(strVal))
+            throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+        return strVal;
     }
-    try {
-        password.value = checkPassword(password.value);
-    } catch (e) {
-        errors.push(e.message);
-    }
-    if(errors.length > 0) {
-        console.log("hitting this")
-        event.preventDefault()
-        let myUL = document.createElement('ul');
-        for (let i = 0; i < errors.length; i++) {
-            let myLi = document.createElement('li');
-            myLi.classList.add('error');
-            myLi.innerHTML = errors[i];
-            myUL.appendChild(myLi);
+    checkName = (name) =>{
+        name = validateString(name)
+        if(name.length > 25 || name.length < 2){
+            throw "name must be between 2 - 25 characters"
         }
-        errorDiv.appendChild(myUL);
-        errorDiv.classList.remove('hidden');
-        errorDiv.hidden = false
+        const regex = /\d/;
+        if(regex.test(name)){
+            throw "name cannot contain numbers"
+        }
+        return name
     }
-    });
-}
+
+    checkUserId = (userId) => {
+        userId = validateString(userId)
+        if(userId.length > 10 || userId.length < 5){
+            throw "UserID must be between 5 - 10 characters"
+        }
+        return userId
+    }
+
+    checkEmail = (email) => {
+        email = validateString(email, "email")
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if(!regex.test(email)){
+            throw "email must be a valid email of the form: example@domain.com"
+        }
+        return email;
+    }
+    checkPassword = (password) => {
+        console.log("in password check")
+        password = validateString(password)
+        if(password.length < 8){
+            throw "Password must be at least 8 characters long"
+        }
+        let regex1 = /[A-Z]/
+        let regex2 = /[^A-Za-z0-9]/
+        let regex3 = /\d/
+        if(!regex1.test(password)){
+            throw "Password must have at least 1 Uppercase letter"
+        }
+        if(!regex2.test(password)){
+            throw "Password must contain at least 1 special character"
+        }
+        if(!regex3.test(password)){
+            throw "Password must have at least 1 number"
+        }
+        return password
+        
+    }
+    
+    let signUpForm = document.getElementById('signup-form');
+    let loginForm = document.getElementById('login-form');
+  
+    if (loginForm) {
+      // We can store references to our elements; it's better to
+      // store them once rather than re-query the DOM traversal each time
+      // that the event runs.
+      let email = document.getElementById('email');
+      let password = document.getElementById('password');
+  
+      let errorDiv = document.getElementById('error-div');
+      let errorTextElement = errorDiv.getElementsByClassName('text-goes-here')[0];
+  
+  
+      // We can take advantage of functional scoping; our event listener has access to its outer functional scope
+      // This means that these variables are accessible in our callback
+      loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+  
+        try {
+          // hide containers by default
+          errorDiv.classList.add('hidden');
+  
+          // Values come from inputs as strings, no matter what :(
+          let userEmail = email.value;
+          let userPassword = password.value;
+          
+          userEmail = checkEmail(userEmail)
+          userPassword = checkPassword(userPassword)
+  
+        } catch (e) {
+          //const message = typeof e === 'string' ? e : e.message;
+          errorTextElement.textContent = e;
+          errorDiv.classList.remove('hidden');
+        }
+      });
+    }
+    if (signUpForm) {
+        console.log("im here")
+        // We can store references to our elements; it's better to
+        // store them once rather than re-query the DOM traversal each time
+        // that the event runs.
+        let firstName = document.getElementById('firstName');
+        let lastName = document.getElementById('lastName');
+        let userId = document.getElementById('userId');
+        let enterEmail = document.getElementById('enterEmail');
+        let password = document.getElementById('password');
+        let confirmPassword = document.getElementById('confirmPassword');
+        
+        let errorDiv = document.getElementById('error-div');
+        let errorTextElement = errorDiv.getElementsByClassName('text-goes-here')[0];
+    
+        
+        // We can take advantage of functional scoping; our event listener has access to its outer functional scope
+        // This means that these variables are accessible in our callback
+        signUpForm.addEventListener('submit', (event) => {
+          event.preventDefault();
+    
+          try {
+            // hide containers by default
+            errorDiv.classList.add('hidden');
+    
+            // Values come from inputs as strings, no matter what :(
+            let userEmail = enterEmail.value;
+            let userPassword = password.value;
+            let name1 = firstName.value
+            let name2 = lastName.value
+            let id = userId.value
+            let confirm = confirmPassword.value
+
+            
+            userEmail = checkEmail(userEmail)
+            userPassword = checkPassword(userPassword)
+            confirm = checkPassword(confirm)
+            if (confirm !== userPassword) {
+                throw 'Error: Password and Confirm Password must match';
+            }
+            name1 = checkName(name1)
+            name2 = checkName(name2)
+            id = checkUserId(id)
+          } catch (e) {
+            //const message = typeof e === 'string' ? e : e.message;
+            errorTextElement.textContent = e;
+            errorDiv.classList.remove('hidden');
+          }
+        });
+      }
+  })();
+  
--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,43 +1,43 @@
-import e, { Router } from 'express';
+import e, { Router } from "express";
 const router = Router();
 import { users, videos } from "../config/mongoCollections.js";
 import bcrypt from "bcrypt";
 import validation from "../utils/validation.js";
 
 const getUserByEmail = async (email) => {
-  //email = validation.checkId(email, "email");
   const userCollection = await users();
   const user = await userCollection.findOne({ email: email });
   if (!user) throw new Error("User not found");
   return user;
-}
+};
 
-router.route('/').get(async (req, res) => {
-  return res.redirect('/intro');
+router.route("/").get(async (req, res) => {
+  return res.redirect("/intro");
+});
+
+router.route("/intro").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    res.redirect("/homepage");
+  } else {
+    return res.render("intro", {
+      title: "Sign Up",
+      layout: "sign_up_layout", // added
+    });
+  }
+});
+
+router.route("/signup").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    res.redirect("/homepage");
+  } else {
+    res.render("sign_up", {
+      layout: "sign_up_layout", // added
+    });
+  }
 });
 
 router
-  .route('/intro')
-  .get(async (req, res) => {
-    if (req.session && req.session.AuthenticationState) {
-      res.redirect("/homepage");
-    } else {
-      return res.render('layouts/intro', { title: "Sign Up" });
-    }
-  });
-
-router
-  .route('/signup')
-  .get(async (req, res) => {
-    if (req.session && req.session.AuthenticationState) {
-      res.redirect("/homepage");
-    } else {
-      res.render('layouts/sign_up_layout');
-    }
-  })
-
-router
-  .route('/auth/logout')
+  .route("/auth/logout")
   .post(async (req, res) => {
     if (req.session && req.session.AuthenticationState) {
       req.session.destroy();
@@ -46,17 +46,18 @@ router
   })
   .get(async (req, res) => {
     return res.redirect("/intro");
-  })
+  });
 
-
-router
-  .route('/auth/login')
-  .post(async (req, res) => {
+router.route("/auth/login").post(async (req, res) => {
+  try {
     const user = await getUserByEmail(req.body.email);
     if (!user) {
       return res.status(401).send("User not found");
     } else {
-      const passwordMatch = await bcrypt.compare(req.body.password, user.password_hash);
+      const passwordMatch = await bcrypt.compare(
+        req.body.password,
+        user.password_hash
+      );
       if (passwordMatch) {
         req.session.AuthenticationState = {
           user: user,
@@ -66,50 +67,56 @@ router
         return res.status(401).redirect("/intro");
       }
     }
-  })
+  } catch (e) {
+    return res.status(401).redirect("/intro");
+  }
+});
 
+router.route("/homepage").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    const v = {}; //await videos.getVideoByOwner( req.session.AuthenticationState.user._id)
+    res.render("homepage", {
+      layout: "main", // added
+      user: req.session.AuthenticationState.user,
+    });
+  } else {
+    return res.status(401).redirect("/intro");
+  }
+});
+
+router.route("/profile").get(async (req, res) => {
+  if (req.session && req.session.AuthenticationState) {
+    res.render("profile", {
+      layout: "main", // added
+      user: req.session.AuthenticationState.user,
+    });
+  } else {
+    return res.status(401).redirect("/intro");
+  }
+});
 
 router
-  .route('/homepage')
+  .route("/settings")
   .get(async (req, res) => {
-    if ((req.session) && (req.session.AuthenticationState)) {
-      const v = {}; //await videos.getVideoByOwner( req.session.AuthenticationState.user._id)
-
-      res.render('layouts/homepage', { user: req.session.AuthenticationState.user});
-    } else {
-      return res.status(401).redirect("/intro");
-    }
-  })
-
-router
-  .route('/profile')
-  .get(async (req, res) => {
-    if ((req.session) && (req.session.AuthenticationState)) {
-      res.render('layouts/profile', { user: req.session.AuthenticationState.user });
-    } else {
-      return res.status(401).redirect("/intro");
-    }
-  })
-
-
-router
-  .route('/settings')
-  .get(async (req, res) => {
-    if ((req.session) && (req.session.AuthenticationState)) {
-      return res.render('layouts/settings', { user: req.session.AuthenticationState.user });
+    if (req.session && req.session.AuthenticationState) {
+      return res.render("settings", {
+        layout: "main", // added
+        user: req.session.AuthenticationState.user,
+      });
     } else {
       return res.status(401).redirect("/intro");
     }
   })
   .post(async (req, res) => {
-    if ((req.session) && (req.session.AuthenticationState)) {
+    if (req.session && req.session.AuthenticationState) {
       users.updateUserPatch(req.session.AuthenticationState.user._id, req.body);
-      res.render('layouts/settings', { user: req.session.AuthenticationState.user });
+      res.render("settings", {
+        layout: "main", // added
+        user: req.session.AuthenticationState.user,
+      });
     } else {
       return res.status(401).redirect("/intro");
     }
-  })
-
-
+  });
 
 export default router;
